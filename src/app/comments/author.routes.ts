b1a0d874f79d64bb0b12c10c5cddee8901542926
Ash Krishan <AKrishan@leighday.root>
2017-02-03 12:45:42 +0000

    Fixed comment bug in author.routes

--- src/app/comments/author.routes.ts
+++ src/app/comments/author.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from "@angular/router";
 
 import { AuthorComponent} from "./author.component";
-import { CommentComponent} from "./comment.component";
+import { CommentsComponent} from "./comments.component";
 
 
 export const AUTHOR_ROUTES: Routes = [
    { path: '', redirectTo: 'list', pathMatch: 'full' },
    { path: 'list', component: AuthorComponent },
-   { path: 'book', component: CommentComponent }
+   { path: 'book', component: CommentsComponent }
 ];
